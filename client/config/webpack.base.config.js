9fee5cd95f9eecde0ced43e7bb45a6394aa9fac8
Code Annihilator <frostberryart@gmail.com>
2017-10-01 16:22:41 +0300

    improved webpack config

--- client/config/webpack.base.config.js
+++ client/config/webpack.base.config.js
@@ -1,71 +1,35 @@
-import path from 'path'
 import webpack from 'webpack'
-import ExtractTextPlugin from 'extract-text-webpack-plugin'
-import HTMLWebpackPlugin from 'html-webpack-plugin'
-
-const IS_PRODUCTION = false
-
-const entry = IS_PRODUCTION
-  ? [ path.join(__dirname, '../src/index.js') ]
-  : [
-    'webpack/hot/dev-server',
-    'webpack-dev-server/client?http://localhost:3000',
-    path.join(__dirname, '../src/index.js')
-  ]
-
-const plugins = IS_PRODUCTION
-  ? [
-    new webpack.optimize.UglifyJsPlugin(),
-    new ExtractTextPlugin('style-[contenthash:10].css'),
-    new HTMLWebpackPlugin({
-      template: path.join(__dirname, '../src/index-template.js')
-    })
-  ]
-  : [ new webpack.HotModuleReplacementPlugin() ]
-
-// plugins.push(
-//   new webpack.DefinePlugin({
-//     'process.env': {
-//         'NODE_ENV': IS_PRODUCTION ? 'production' : ''
-//     }
-//   })
-// )
-
-const cssIdentifier = IS_PRODUCTION ? '[hash:base64:10]' : '[path][name]---[local]'
-
-const cssLoader = IS_PRODUCTION
-  ? ExtractTextPlugin.extract({ loader: 'css-loader?minimize&localIdentName=' + cssIdentifier })
-  : [ 'style-loader', 'css-loader?localIdentName=' + cssIdentifier ]
+import { SRC, NODE_MODULES } from './paths'
 
 export default {
-  devtool: 'inline-source-map',
-  entry: entry,
-  plugins: plugins,
   resolve: {
     alias: {
-      '@src': path.resolve(__dirname, '../src'),
-      '@images': path.resolve(__dirname, '../src/assets/images')
+      '@src': SRC,
+      '@images': `${SRC}/assets/images`,
+      '@rc-slider': `${NODE_MODULES}/rc-slider/assets`
     },
     extensions: ['.js']
   },
+  plugins: [
+    new webpack.optimize.ModuleConcatenationPlugin()
+  ],
   module: {
-		loaders: [{
+    rules: [{
 			test: /\.js$/,
-			loaders: ['babel-loader'],
+			use: [{
+        loader: 'babel-loader'
+      }],
 			exclude: /node_modules/
 		}, {
 			test: /\.(png|jpg|gif)$/,
-			loaders: ['url-loader?limit=10000&name=images/[hash:12].[ext]'],
-			exclude: /node_modules/
-		}, {
-			test: /\.css$/,
-			loaders: cssLoader,
+			use: [{
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: 'images/[hash:12].[ext]'
+        }
+      }],
 			exclude: /node_modules/
 		}]
-	},
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-		publicPath: IS_PRODUCTION ? '/' : '/dist/',
-		filename: IS_PRODUCTION ? 'bundle.[hash:12].min.js' : 'bundle.js'
 	}
 }
--- client/config/webpack.development.config.js
+++ client/config/webpack.development.config.js
@@ -3,47 +3,39 @@ import webpack from 'webpack'
 import ExtractTextPlugin from 'extract-text-webpack-plugin'
 import HTMLWebpackPlugin from 'html-webpack-plugin'
 
+import webpackBaseConfig from './webpack.base.config'
+import { SRC } from './paths'
+
 export default {
   devtool: 'inline-source-map',
   entry: [
     'webpack/hot/dev-server',
-    'webpack-dev-server/client?http://localhost:3000',
-    path.join(__dirname, '../src/index.js')
+    `webpack-dev-server/client?${process.env.FETCHURL}:${process.env.PORT}`,
+    `${SRC}/index.js`
   ],
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.DefinePlugin({
-      process: {
-        env: {
-          NODE_ENV: JSON.stringify('development')
-        }
-      }
-    })
+    new webpack.DefinePlugin({ 'process.env': { 'NODE_ENV': JSON.stringify('development') } }),
+    ...webpackBaseConfig.plugins
   ],
-  resolve: {
-    alias: {
-      '@src': path.resolve(__dirname, '../src'),
-      '@images': path.resolve(__dirname, '../src/assets/images'),
-      '@rc-slider': path.resolve(__dirname, '../../node_modules/rc-slider/assets/')
-    },
-    extensions: ['.js']
-  },
+  resolve: webpackBaseConfig.resolve,
   module: {
-		loaders: [{
-			test: /\.js$/,
-			loaders: ['babel-loader'],
-			exclude: /node_modules/
-		}, {
-			test: /\.(png|jpg|gif)$/,
-			loaders: ['url-loader?limit=10000&name=images/[hash:12].[ext]']
-		}, {
+		rules: [{
 			test: /\.s?css$/,
       exclude: /module\.s?css$/,
-			loaders: [ 'style-loader', 'css-loader?modules=false&localIdentName=[path][name]---[local]!sass-loader' ],
+      use: [
+        { loader: 'style-loader' },
+        { loader: 'css-loader', options: { modules: false, localIdentName: '[path][name]---[local]' } },
+        { loader: 'sass-loader' }
+      ]
 		},{
 			test: /module\.s?css$/,
-			loaders: [ 'style-loader', 'css-loader?modules=true&localIdentName=[path][name]---[local]!sass-loader' ]
-		}]
+      use: [
+        { loader: 'style-loader' },
+        { loader: 'css-loader', options: { modules: true, localIdentName: '[path][name]---[local]' } },
+        { loader: 'sass-loader' }
+      ]
+		}, ...webpackBaseConfig.module.rules]
 	},
   output: {
     path: path.resolve(__dirname, 'dist'),
